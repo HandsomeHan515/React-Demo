3d3ec3cf45f886c2cb3ac6f6e2e9b84b941a0850
hanshuai <hanshuai@dongwu-inc.com>
2018-01-31 11:15:00 +0800

    set body minheight

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Header, Footer } from './components';
 
 const App = props => {
+  console.log('height: %o', document.body.clientHeight)
   return (
     <div>
       <Header />
-      {props.children}
+      <div style={{ minHeight: document.body.clientHeight - 110 }}>
+        {props.children}
+      </div>
       <Footer />
     </div>
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import '../less/global.less';
+import '../less/footer.less';
 
 export default function footer() {
   return (
-    <div className="center">
+    <div className="footer center">
       This is Footer!
     </div>
   )
