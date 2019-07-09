b560e1a253ec31169fdbacf85e98caffa6e241af
hanshuai <hanshuai@baiwang.com>
2019-07-09 15:29:13 +0800

    file rename

---
+++ src/containers/notFound/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../less/global.less';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="center">
+        404 NotFound
+      </div>
+    );
+  }
+}
+
+export default NotFound;
